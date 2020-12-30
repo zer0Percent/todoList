3cf9a556e20ba8d328bc8cede437cd02c0800bc4
zer0Percent <48766248+zer0Percent@users.noreply.github.com>
2020-12-30 19:20:43 +0100

    git ignore and starting angular proyect

---
+++ todolist/frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskComponent } from '../app/components/task/task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todolist/frontend/src/app/models/task.ts
@@ -0,0 +1,13 @@
+
+
+/**
+ * A Task will have two fields:
+ * Status: integer that represents the status of a Task. 0 corresponds with 'to-do'. 1 with 'completed'
+ * Description: string that represents the description of a Task
+ */
+export class Task {
+
+    public status: number;
+    public description: string;
+
+}
