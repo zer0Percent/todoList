766663fc184aeb5f2e80ec3e687aa5626fd4ab5b
zer0Percent <48766248+zer0Percent@users.noreply.github.com>
2021-01-01 21:18:55 +0100

    Architecture implemented
    Defined a Repository of Tasks a their three methods.
    Defined a N-layer architecture
    Using a SQLITE database to simplify (and because I'm using macOs and didn't find SQL server Management Studio)

--- Backend/PerfectChannel.WebApi/Controllers/TaskController.cs
+++ Backend/PerfectChannel.WebApi/Controllers/TaskController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Cors;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using ApplicationLayer.Services;
+using DomainLayer;
 
 namespace PerfectChannel.WebApi.Controllers
 {
@@ -8,6 +11,47 @@ namespace PerfectChannel.WebApi.Controllers
     [ApiController]
     public class TaskController : ControllerBase
     {
+        [HttpGet("something", Name = "something")]
+        public string some()
+        {
+            return "algo";
+        }
         // TODO: to be completed
+        [HttpGet("GetTasks", Name = "GetTasks")]
+        public IEnumerable<Task> GetTasks()
+        {
+            IEnumerable<Task> tasks = new List<Task>();
+            using(TaskApplicationService taskService = new TaskApplicationService())
+            {
+                tasks = taskService.GetTasks();
+            }
+
+            return tasks;
+        }
+
+        [HttpPost("AddTask", Name = "AddTask")]
+        public bool AddTask(Task newTask)
+        {
+            bool result = false;
+            using(TaskApplicationService taskService = new TaskApplicationService())
+            {
+                result = taskService.PostTask(newTask);
+            }
+            return result;
+        }
+
+        [HttpPut("ModifyTask", Name = "ModifyTask")]
+        public bool ModifyTask(Task taskToModify)
+        {
+            bool result = false;
+            using(TaskApplicationService taskService = new TaskApplicationService())
+            {
+                result = taskService.ModifyTask(taskToModify);
+            }
+
+            return result;
+        }
+
+
     }
 }
